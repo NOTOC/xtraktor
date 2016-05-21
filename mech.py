6d4a2a74df82fcece6193194a8f26a5a0dcf4362
God <ravnjakivan@gmail.com>
2016-05-22 00:35:00 +0200

    git commit

---
+++ mech.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import mechanize
+#apt-get install python-mechanize
+import cookielib
+
+# http://stockrt.github.io/p/emulating-a-browser-in-python-with-mechanize/
+
+def openURL(websiteURL):
+    ## setup browser
+
+    # Browser
+    br = mechanize.Browser()
+
+    # Cookie Jar
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
+
+    # Browser options
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(False)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+
+    # Follows refresh 0 but not hangs on refresh > 0
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+    # Want debugging messages?
+    #br.set_debug_http(True)
+    #br.set_debug_redirects(True)
+    #br.set_debug_responses(True)
+
+    # User-Agent (this is cheating, ok?)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    
+    ## open url
+    r = br.open("http://"+websiteURL)
+    html = r.read()
+
+    # Show the source
+    print html
+    # or
+    print br.response().read()
+
+    # Show the html title
+    print br.title()
+
+    # Show the response headers
+    print r.info()
+    # or
+    print br.response().info()
+
+    # Show the available forms
+    for f in br.forms():
+        print f
+    
+    return r.info()
+    
+    
+#print openURL("www.index.hr")
+
+
+
+
+
+
+
+
+
